5e77e615f827e1347325c71b398f449b66d4f7d5
김준영 <1090jun@naver.com>
2025-05-27 17:09:48 +0900

    0527_add_tasks_front

---
+++ APPs/tasks_app/front/src/components/Common/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { navMenus } from "../../utils/menuList";
+import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+
+const Navbar = ({ activeIdx }) => {
+  return (
+    <nav className="bg-[#212121] w-1/5 h-full rounded-sm border border-gray-500 py-10 px-4 flex flex-col justify-between items-center">
+      <div className="logo-wrapper flex w-full justify-center items-center gap-8">
+        <div className="logo"></div>
+        <h2 className="font-semibold text-xl">
+          <Link to="/">JY</Link>
+        </h2>
+      </div>
+      <ul className="menus">
+        {navMenus.map((menu, idx) => (
+          <li
+            key={idx}
+            className={`rounded-sm mb-1 border border-gray-700 hover:bg-gray-950 transition-all duration-300 ${
+              menu.idx === activeIdx ? "bg-gray-950" : ""
+            }`}
+          >
+            <Link to={menu.to} className="flex gap-x-4 items-center py-2 px-10">
+              {menu.icon}
+              {menu.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <div className="auth-wrapper flex justify-center w-4/5">
+        <button className="flex justify-center items-center gap-2 bg-gray-300 text-gray-900 py-3 px-4 rounded-md w-full">
+          <FcGoogle />
+          <span className="text-sm">Google Login</span>
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
