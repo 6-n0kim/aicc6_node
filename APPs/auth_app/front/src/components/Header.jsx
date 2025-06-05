2933bc67a5d5ce78482d2ad44b72d3dff8fa34cc
김준영 <1090jun@naver.com>
2025-06-05 12:18:55 +0900

    0605_front_setting

---
+++ APPs/auth_app/front/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <p>id : 1</p>
+        <p>username : jy</p>
+        <p>email : asdf@gmail.com</p>
+        <button>LOGOUT</button>
+      </div>
+    </div>
+  );
+};
+export default Header;
